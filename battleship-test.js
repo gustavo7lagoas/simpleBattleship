558590fef4f7de0cf069bbe0a9cb4a0072dd4d7e
gustavo7lagoas <gustavo7lagoas@gmail.com>
2015-04-12 18:06:02 -0300

    first tests for board and battleship

---
+++ battleship-test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Battleship = require('./battleship.js');
+
+casper.test.begin('Can populate its board with ships', 1, function suite(test) {
+    var myBattleship = new Battleship(2, 4, 2);
+    myBattleship.populateBoard();
+    var expectBoard = [['x', 'x'],['x', 'x']];
+    test.assertEquals(myBattleship.board.board, expectBoard, 'boards are equally populated');
+    test.done();
+});
---
+++ board-test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var Board = require('./board.js');
+
+casper.test.begin('Can create a board of size 10', 2, function suite(test) {
+    var myBoard = new Board(10, 10);
+    test.assertEquals(myBoard.board[0].length, 10, 'Column number ok');
+    test.assertEquals(myBoard.board.length, 9, 'Line number ok');
+    test.done();
+});
