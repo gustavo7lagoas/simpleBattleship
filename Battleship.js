16b91fee9afd721e18983625e2fc43b203ddc333
gustavo7lagoas <gustavo7lagoas@gmail.com>
2015-04-13 22:34:00 -0300

    adds player guess function

--- Battleship.js
+++ Battleship.js
@@ -1,5 +1,6 @@
 'user strict';
 var Board = require('./Board.js');
+var readline = require('readline');
 
 function Battleship(playsNumber, shipsNumber, boardSize) {
     this.playsNumber = playsNumber;
@@ -14,14 +15,31 @@ Battleship.prototype = {
         while(shipsPlaced !== this.shipsNumber) {
             linePos = Math.floor(Math.random() * this.board.linesNumber);
             columnPos = Math.floor(Math.random() * this.board.columnsNumber);
-            this.board.board[linePos][columnPos] = 'x';
-            this.board.prettyPrint();
-            shipsPlaced += 1;
+            if(this.board.board[linePos][columnPos] !== 'x') {
+                var position = new Array;
+                position = [linePos, columnPos];
+                this.board.board[linePos][columnPos] = 'x';
+                shipsPlaced += 1;
+                this.shipPositions.push(position);
+            }
         }
+    },
+    playerGuess : function() {
+        var guessNumber = 0;
+        var rl = readline.createInterface({
+            input : process.stdin,
+            output : process.stdout
+        });
+        rl.question('Enter guess', function(guess) {
+            console.log('Guess #', guessNumber);
+            guessNumber += 1;
+            rl.close();
+        });
     }
 };
 
-var myBattleship = new Battleship(2, 4, 2);
+var myBattleship = new Battleship(2,4,2);
 myBattleship.populateBoard();
+myBattleship.playerGuess();
 
 module.exports = Battleship;
