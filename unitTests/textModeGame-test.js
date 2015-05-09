3ef98760d7a40c5358013a1c127dd832a30faede
gustavo7lagoas <gustavo7lagoas@gmail.com>
2015-05-09 18:41:57 -0300

    adds tests for set configurations

--- unitTests/textModeGame-test.js
+++ unitTests/textModeGame-test.js
@@ -5,14 +5,38 @@ var should = require('chai').should;
 var TextModeGame = require('../TextModeGame');
 var _ = require('lodash');
 var sinon = require('sinon');
+var myTextModeGame;
+var mySeaSize;
+var myDifficultyLevel;
+
+beforeEach(function(done) {
+    myTextModeGame = new TextModeGame();
+    done();
+});
 
 describe('Text Mode Game', function() {
     describe('_getConfiguration', function() {
-        it('sets a valid configuration', function() {
-            true.should.be.false;
+        describe('_setSeaSize', function() {
+            it('normalizes a lower case configuration', function() {
+                mySeaSize = 'm';
+                myTextModeGame._setSeaSize(mySeaSize).should.equal('M');
+            });
+            it('normalizes a invalid configuration to Small', function() {
+                mySeaSize = 'e';
+                myTextModeGame._setSeaSize(mySeaSize).should.equal('S');
+            });
         });
-        it('should set lower configuration when invalid configuration is set', function() {
-            true.should.be.false;
+        describe('_setDifficultyLevel', function() {
+            it('normalizes a lower case configuration', function() {
+                mySeaSize = 'S';
+                myDifficultyLevel = 'h';
+                myTextModeGame._setDifficultyLevel(mySeaSize, myDifficultyLevel).should.equal('H');
+            });
+            it('normalizes a invalid configuration to Easy', function() {
+                mySeaSize = 'L';
+                myDifficultyLevel = 'normal';
+                myTextModeGame._setDifficultyLevel(mySeaSize, myDifficultyLevel).should.equal('E');
+            });
         });
     });
     describe('isPlayerWin', function() {
