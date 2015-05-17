02513b8631b4b92056c52625137a67eeb1b84eee
gustavo7lagoas <gustavo7lagoas@gmail.com>
2015-05-17 19:26:25 -0300

    acceptance tests wip

--- acceptanceTests/textModeGame-test.js
+++ acceptanceTests/textModeGame-test.js
@@ -1,7 +1,7 @@
 /*jshint node:true*/
 'use strict';
 
-var should = require('chai').should;
+var should = require('chai').should();
 var TextModeGame = require('../TextModeGame');
 var _ = require('lodash');
 var sinon = require('sinon');
@@ -22,11 +22,14 @@ describe('Text Mode Game', function() {
                 myTextModeGame.shipsNumber.should.equal(1);
             });
             it('In Normal difficult it has 3 plays and 2 ships', function() {
-                myTextModeGame._setDifficultyLevel('S', 'E');
+                myTextModeGame._setDifficultyLevel('S', 'N');
+                myTextModeGame.playsNumber.should.equal(3);
                 myTextModeGame.shipsNumber.should.equal(2);
             });
             it('In Hard difficult it has 3 plays and 3 ships', function() {
-                true.should.be.false;
+                myTextModeGame._setDifficultyLevel('S', 'H');
+                myTextModeGame.playsNumber.should.equal(3);
+                myTextModeGame.shipsNumber.should.equal(3);
             });
         });
         context('Mid-size Sea Size', function(){
@@ -34,13 +37,19 @@ describe('Text Mode Game', function() {
                 myTextModeGame._setSeaSize('M');
             });
             it('In Easy difficult it has 22 plays and 5 ships', function() {
-                true.should.be.false;
+                myTextModeGame._setDifficultyLevel('M', 'E');
+                myTextModeGame.playsNumber.should.equal(22);
+                myTextModeGame.shipsNumber.should.equal(5);
             });
             it('In Normal difficult it has 20 plays and 8 ships', function() {
-                true.should.be.false;
+                myTextModeGame._setDifficultyLevel('M', 'N');
+                myTextModeGame.playsNumber.should.equal(20);
+                myTextModeGame.shipsNumber.should.equal(8);
             });
             it('In Hard difficult it has 13 plays and 10 ships', function() {
-                true.should.be.false;
+                myTextModeGame._setDifficultyLevel('M', 'H');
+                myTextModeGame.playsNumber.should.equal(13);
+                myTextModeGame.shipsNumber.should.equal(10);
             });
         });
         context('Large Sea Size', function(){
@@ -48,17 +57,28 @@ describe('Text Mode Game', function() {
                 myTextModeGame._setSeaSize('L');
             });
             it('In Easy difficult it has 58 plays and 10 ships', function() {
-                true.should.be.false;
+                myTextModeGame._setDifficultyLevel('L', 'E');
+                myTextModeGame.playsNumber.should.equal(58);
+                myTextModeGame.shipsNumber.should.equal(10);
             });
             it('In Normal difficult it has 54 plays and 14 ships', function() {
-                true.should.be.false;
+                myTextModeGame._setDifficultyLevel('L', 'N');
+                myTextModeGame.playsNumber.should.equal(54);
+                myTextModeGame.shipsNumber.should.equal(14);
             });
             it('In Hard difficult it has 30 plays and 20 ships', function() {
-                true.should.be.false;
+                myTextModeGame._setDifficultyLevel('L', 'H');
+                myTextModeGame.playsNumber.should.equal(30);
+                myTextModeGame.shipsNumber.should.equal(20);
             });
         });
     });
     describe('Game Play', function() {
+        beforeEach(function() {
+            myTextModeGame = new TextModeGame();
+            myTextModeGame._setSeaSize('S');
+            myTextModeGame._setDifficultyLevel('S', 'E');
+        });
         context('Player Wins', function() {
             it('player wins with the minimal number of plays', function() {
                 true.should.be.false;
@@ -71,6 +91,6 @@ describe('Text Mode Game', function() {
             it('no more attempts', function(){
                 true.should.be.false;
             });
-        })
+        });
     });
 });
