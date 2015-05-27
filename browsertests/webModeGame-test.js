3b82e2dc858222dd5bbd0799210f89fd552ded6d
alexwarmling <alex@chaordicsystems.com>
2015-05-27 17:35:53 -0300

    Include web automation tests

--- browsertests/webModeGame-test.js
+++ browsertests/webModeGame-test.js
@@ -4,12 +4,12 @@
 
 debugger;
 
-var gamepath = 'http://localhost:8080/WebModeGame.html';
+var gamepath = 'file:///home/alex/workspace/qaschool/academia/simpleBattleship/WebModeGame.html';
 
-casper.test.begin('Teste', 1, function battleship(test){
+casper.test.begin('Check if game is loaded', 1, function battleship(test){
 
 	casper.start(gamepath, function(){
-		test.assertTitle("Example Domain", "Battleship title is the one expected");
+		test.assertTitle("Battleship Game", "Battleship title is the one expected");
 	});
 
 	casper.run(function() {
@@ -17,17 +17,32 @@ casper.test.begin('Teste', 1, function battleship(test){
     });
 });
 
-casper.test.begin('Start Game', 1, function battleship(test){
+casper.test.begin('Start Game', 4, function battleship(test){
 
 	casper.start(gamepath, function(){
-        this.fill('form', {
-            'sea-size' : 'L',
-            'difficulty-level' : 'N'
-        }, true);
-	});
+
+        casper.then(function() {
+            
+            this.evaluate(function(term) {
+                document.querySelector('input[name=sea-size][value=S]').setAttribute('checked', true);
+                document.querySelector('input[name=difficulty-level][value=N]').setAttribute('checked', true);
+            });
+            
+            test.assertEval(function() {
+                return document.querySelector('input[name=sea-size][value=S]').getAttribute('checked') == "true";
+            }, 'Sea-Size was set to S');
+            
+            test.assertEval(function() {
+                return document.querySelector('input[name=difficulty-level][value=N]').getAttribute('checked') == "true";
+            }, 'Difficulty level was set to N');
+            
+            this.click('#start');
+        });
+    });
 
     casper.then(function() {
         test.assertExists('.column', 'Did not create any Board Column');
+        test.assertExists('.row', 'Did not create any Board Row');
     });
 
 	casper.run(function() {
